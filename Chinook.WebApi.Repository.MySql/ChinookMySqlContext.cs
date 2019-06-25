52fef34a9f272b8ff2b8808fe3aa578a4ca1bad6
becker oliver utia lopez <becker_utia_123@hotmail.com>
2019-06-25 09:05:26 -0500

    first step

---
+++ Chinook.WebApi.Repository.MySql/ChinookMySqlContext.cs
@@ -0,0 +1,25 @@
+﻿using Chinook.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chinook.WebApi.Repository.MySql
+{
+    public class ChinookMySqlContext : DbContext
+
+    {
+        public ChinookMySqlContext(DbContextOptions<ChinookMySqlContext> options)
+          : base(options)
+        {
+        }
+        public virtual DbSet<Album> Album { get; set; }
+        public virtual DbSet<Artist> Artist { get; set; }
+        public virtual DbSet<Customer> Customer { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
+        public virtual DbSet<Genre> Genre { get; set; }
+        public virtual DbSet<Invoice> Invoice { get; set; }
+        public virtual DbSet<InvoiceLine> InvoiceLine { get; set; }
+        public virtual DbSet<MediaType> MediaType { get; set; }
+        public virtual DbSet<Playlist> Playlist { get; set; }
+        public virtual DbSet<PlaylistTrack> PlaylistTrack { get; set; }
+        public virtual DbSet<Track> Track { get; set; }
+    }
+}
